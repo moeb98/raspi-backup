efc87e42938ebeb9bae14d05e54b0770f318fb67
Mario Eberlein <moeb98@yahoo.de>
2021-04-21 19:43:00 +0100

    added support for command line arguments

--- raspi-backup.sh
+++ raspi-backup.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
-# mount hard drive in case that is required
-mount -t cifs -o credentials=/etc/smbcredentials,rw,file_mode=0777,dir_mode=0777 //192.168.1.2/Public /mnt/nas
-
 # variables
 BACKUP_PATH="/mnt/nas/raspi-backup"
-BACKUP_NUMBER="5"
+BACKUP_REVISIONS="5"
 BACKUP_NAME="raspi-backup"
+DO_MOUNT=false;
+
+# read command line arguments
+while getopts n:r:p:m: flag
+do
+    case "${flag}" in
+        n) BACKUP_NAME=${OPTARG};;
+        r) BACKUP_REVISIONS=${OPTARG};;
+        p) BACKUP_PATH=${OPTARG};;
+	m) DO_MOUNT=${OPTARG};;
+    esac
+done
+
+if [ $DO_MOUNT == true ]; then
+    # mount hard drive in case that is required
+    echo "Mounting ..."
+    mount -t cifs -o credentials=/etc/smbcredentials,rw,file_mode=0777,dir_mode=0777 //192.168.1.2/Public /mnt/nas
+fi
 
 # perform backup
 dd if=/dev/mmcblk0 of=${BACKUP_PATH}/${BACKUP_NAME}-$(date +%Y%m%d).img bs=1MB
 
 # delete old backups and keep BACKUP_NUMBER of backups only
-pushd ${BACKUP_PATH}; ls -tr ${BACKUP_PATH}/${BACKUP_NAME}* | head -n -${BACKUP_NUMBER} | xargs rm; popd
+pushd ${BACKUP_PATH}; ls -tr ${BACKUP_PATH}/${BACKUP_NAME}* | head -n -${BACKUP_REVISIONS} | xargs rm; popd
 
-# unmount hard drive in case that is required
-umount /mnt/nas
+if [ $DO_MOUNT == true ]; then
+    echo "Unmounting ... "
+    # unmount hard drive in case that is required
+    umount /mnt/nas
+fi
